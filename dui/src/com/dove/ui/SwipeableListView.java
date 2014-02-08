0f00d4c4e2b12eec11d6bb811494b559b374967b
zhifuzhang <george.zhang.work@gmail.com>
2014-02-08 15:30:22 +0800

    Add dui library.

---
+++ dui/src/com/dove/ui/SwipeableListView.java
@@ -0,0 +1,22 @@
+
+package com.dove.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class SwipeableListView extends ListView {
+
+    public SwipeableListView(Context context) {
+        super(context);
+    }
+
+    public SwipeableListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SwipeableListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+}
