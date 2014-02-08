10ccaef37fb0b6cbb3782d1fec9fc804a920bbfc
zhifuzhang <george.zhang.work@gmail.com>
2014-02-08 19:33:55 +0800

    Add the logutils extract from google mail library.

---
+++ dlog/src/com/dove/log/LogTag.java
@@ -0,0 +1,20 @@
+package com.dove.log;
+
+public class LogTag {
+    private static String sLogTag = "UnifiedEmail";
+
+    /**
+     * Get the log tag to apply to logging.
+     */
+    public static String getLogTag() {
+        return sLogTag;
+    }
+
+    /**
+     * Sets the app-wide log tag to be used in most log messages, and for enabling logging
+     * verbosity. This should be called at most once, during app start-up.
+     */
+    public static void setLogTag(final String logTag) {
+        sLogTag = logTag;
+    }
+}
