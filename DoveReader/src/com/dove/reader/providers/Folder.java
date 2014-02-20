588a2961919dd529fd6f5eb47ff07d44c40f5a3e
mebusw <mebusw@163.com>
2014-02-20 12:04:48 +0800

    Add the object cursor: A cursor-backed type that can return an object for each row of the cursor. This class is most usefull when:
    The cursor is returned in conjuction with an AsyncTaskLoader and created off the UI thread.
    A single row in the cursor specifies everything for an object.

--- DoveReader/src/com/dove/reader/providers/Folder.java
+++ DoveReader/src/com/dove/reader/providers/Folder.java
@@ -1,17 +1,30 @@
 
 package com.dove.reader.providers;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * A fold is a collection of book categories, and perhaps other folders.
  */
-// TODO The following notes should be removed from the source code.
-/**
- * <p>
- * 按照Gmail的关于Folder的概念来组织管理各类书籍，书籍的分类。 这里Folder的概念可以认为是：
- * </p>
- * <ol>
- * <li>1. 书籍的种类。如小说，儿童读物。</li> 2. 具体的种类，如小说当中的玄幻小说（按题材分类）
- */
-public class Folder {
+public class Folder implements Parcelable, Comparable<Folder> {
+
+    @Override
+    public int describeContents() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int compareTo(Folder another) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
 }
