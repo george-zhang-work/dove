20fe25be9fba99a2208478465cff0855faa72d18
zhifuzhang <george.zhang.work@gmail.com>
2014-02-20 08:27:31 +0800

    Add the fold concept.

---
+++ DoveReader/src/com/dove/reader/providers/Folder.java
@@ -0,0 +1,17 @@
+
+package com.dove.reader.providers;
+
+/**
+ * A fold is a collection of book categories, and perhaps other folders.
+ */
+// TODO The following notes should be removed from the source code.
+/**
+ * <p>
+ * 按照Gmail的关于Folder的概念来组织管理各类书籍，书籍的分类。 这里Folder的概念可以认为是：
+ * </p>
+ * <ol>
+ * <li>1. 书籍的种类。如小说，儿童读物。</li> 2. 具体的种类，如小说当中的玄幻小说（按题材分类）
+ */
+public class Folder {
+
+}
