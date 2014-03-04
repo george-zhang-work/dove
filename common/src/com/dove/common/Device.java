998725b885235e678e4f4244a06fb46d5b4ac8d4
zhifuzhang <george.zhang.work@gmail.com>
2014-03-05 01:44:05 +0800

    Add the utility class to get device id.

---
+++ common/src/com/dove/common/Device.java
@@ -0,0 +1,12 @@
+
+package com.dove.common;
+
+import android.content.Context;
+
+public final class Device {
+    private static String sDeviceId = null;
+
+    public synchronized static String getDeviceId(Context context) {
+        
+    }
+}
