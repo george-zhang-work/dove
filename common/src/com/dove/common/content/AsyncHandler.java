3116f78243ffad582bf76de385dfe93ab9988b29
mebusw <george.zhang.work@gmail.com>
2014-03-14 11:12:30 +0800

    AsyncHandler: copy the AsyncHandler from google's deskclock com.android.deskclock.AsyncHandler. Helper class for managing the background thread used to perform io operations and handle async broadcasts.

---
+++ common/src/com/dove/common/content/AsyncHandler.java
@@ -0,0 +1,35 @@
+
+package com.dove.common.content;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+
+/**
+ * Helper class for managing the background thread used to perform io operations
+ * and handle async broadcasts.
+ */
+public class AsyncHandler {
+    private static final HandlerThread sHandlerThread = new HandlerThread("AsyncHandler");
+    private static final Handler sHandler;
+
+    static {
+        sHandlerThread.start();
+        sHandler = new Handler(sHandlerThread.getLooper());
+    }
+
+    /**
+     * Causes the Runnable r to be added to the message queue. The runnable will
+     * be run on the background thread to which inner handler is attached.
+     * 
+     * @param r The Runnable that will be executed.
+     * @return Returns true if the Runnable was successfully placed in to the
+     *         message queue. Returns false on failure, usually because the
+     *         looper processing the message queue is exiting.
+     */
+    public static boolean post(Runnable r) {
+        return sHandler.post(r);
+    }
+
+    private AsyncHandler() {
+    }
+}
