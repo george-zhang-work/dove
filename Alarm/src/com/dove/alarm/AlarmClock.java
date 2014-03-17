283db227df961a5668be959d82ddc1a566e5103e
mebusw <george.zhang.work@gmail.com>
2014-03-17 16:41:58 +0800

    Add AlarmmClock file: This file is used to do response when a alarm with type clock arrived.

---
+++ Alarm/src/com/dove/alarm/AlarmClock.java
@@ -0,0 +1,17 @@
+
+package com.dove.alarm;
+
+public class AlarmClock {
+
+    public AlarmClock() {
+    }
+
+    /**
+     * Try to pop clock window before the TTS (Text-To-Speech).
+     * 
+     * @return True if the the window popup successfully, false not.
+     */
+    public boolean tryToPopupWindow() {
+        return false;
+    }
+}
